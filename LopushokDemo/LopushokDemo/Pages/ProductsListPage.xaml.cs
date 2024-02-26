5897bafe9812348f9f9c465971d1a6ae4066cccd
riazonbin <riazonbin@mail.ru>
2024-02-26 18:15:29 +0300

    Добавлен ico файл, добавлена логика для фильтрации, сортировки, поиска товаров

--- LopushokDemo/LopushokDemo/Pages/ProductsListPage.xaml.cs
+++ LopushokDemo/LopushokDemo/Pages/ProductsListPage.xaml.cs
@@ -21,6 +21,15 @@ namespace LopushokDemo.Pages
     /// </summary>
     public partial class ProductsListPage : Page
     {
+        private const int PageSize = 20;
+        private int CurrentPage = 0;
+        private int TotalCount;
+        private int PagesCount;
+        private string FilterName;
+        private string SearchFilter = "";
+        private string SortField;
+        private bool IsAsc = false;
+
         private List<Product> _products = new List<Product>();
         private Func<Product, object> _sortingQuery = x => x.Name;
         private Predicate<Product> _filterQuery = x => true;
@@ -30,47 +39,133 @@ namespace LopushokDemo.Pages
             InitializeComponent();
             _products = App.Connection.Product.ToList();
             lvProducts.ItemsSource = _products;
+        }
 
+        private void cbSortOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var order = (string)((ComboBox)sender).SelectedItem;
+            if (order == "По возрастанию")
+            {
+                IsAsc = true;
+            }
+            else
+            {
+                IsAsc = false;
+            }
+            UpdateSource();
+        }
+
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SortField = (string)((ComboBox)sender).SelectedItem;
+            UpdateSource();
+        }
+
+        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = (ProductType)((ComboBox)sender).SelectedItem;
+            if (selectedItem == null)
+                return;
+            FilterName = selectedItem.Name;
+            UpdateSource();
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text = ((TextBox)sender).Text;
+            SearchFilter = text;
+            UpdateSource();
+        }
+
+        private void PrevPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage = Math.Max(0, CurrentPage - 1);
+            UpdateSource();
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage = Math.Min(PagesCount - 1, CurrentPage + 1);
+            UpdateSource();
+        }
+
+        private void PagesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = ((ListBox)sender).SelectedItem;
+            var pageNumber = (int)listBox - 1;
+            CurrentPage = pageNumber;
+            UpdateSource();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
             var filters = new List<ProductType> { new ProductType { Name = "Все типы" } };
             filters.AddRange(App.Connection.ProductType.ToList());
             cbFilter.ItemsSource = filters;
             cbFilter.SelectedIndex = 0;
 
             var sorts = new List<string> {
-            "Без сортировки",
-             "Наименование", "Номер производственного цеха ",
-            "Минимальная стоимость для агента"};
+                    "Без сортировки",
+                    "Наименование", "Номер производственного цеха",
+                    "Минимальная стоимость для агента"};
             cbSort.ItemsSource = sorts;
             cbSort.SelectedIndex = 0;
 
             cbSortOrders.ItemsSource = new List<string> { "По убыванию", "По возрастанию" };
             cbSortOrders.SelectedIndex = 0;
+            UpdateSource();
         }
 
-        private void ReloadData()
+        private void UpdateSource()
         {
-            _products = App.Connection.Product
-            .ToList()
-            .Where(x => _filterQuery(x))
-                .OrderBy(x => _sortingQuery(x))
-                .ToList();
-            SearchData();
+            List<Product> products;
+            if (string.IsNullOrEmpty(FilterName) || FilterName == "Все типы")
+            {
+                products = App.Connection.Product.ToList();
+            }
+            else
+            {
+                products = App.Connection.Product.ToList().Where(x => x.ProductType.Name == FilterName).ToList();
+            }
+
+            products = products.Where(x => x.Name.ToLower().Contains(SearchFilter.ToLower())).ToList();
+
+            TotalCount = products.Count;
+            PagesCount = TotalCount / PageSize + (TotalCount % PageSize != 0 ? 1 : 0);
+            PagesListBox.ItemsSource = Enumerable.Range(0, PagesCount).Select(x => x + 1);
+
+            if (IsAsc)
+            {
+                if (SortField == "Наименование")
+                {
+                    products = products.OrderBy(x => x.Name).ToList();
+                }
+                else if (SortField == "Номер производственного цеха")
+                {
+                    products = products.OrderBy(x => x.NumberOfFactory).ToList();
+                }
+                else if (SortField == "Минимальная стоимость для агента")
+                {
+                    products = products.OrderBy(x => x.MinimalAgentCost).ToList();
+                }
+            }
+            else
+            {
+                if (SortField == "Наименование")
+                {
+                    products = products.OrderByDescending(x => x.Name).ToList();
+                }
+                else if (SortField == "Номер производственного цеха")
+                {
+                    products = products.OrderByDescending(x => x.NumberOfFactory).ToList();
+                }
+                else
+                {
+                    products = products.OrderByDescending(x => x.MinimalAgentCost).ToList();
+                }
+            }
+            lvProducts.ItemsSource = products.Skip(PageSize * CurrentPage).Take(PageSize);
         }
 
-        private void SearchData()
-        {
-            lvProducts.ItemsSource = _products
-                .Where(x => string.Join(" ", x.Name).ToLower().Contains(tbSearch.Text.ToLower()));
-        }
-
-        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            SearchData();
-        }
-
-        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
     }
 }
