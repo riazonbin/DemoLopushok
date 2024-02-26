fa5c300467a6c10f88e4754dbc6fbd3e82b97d7a
riazonbin <riazonbin@mail.ru>
2024-02-26 17:55:02 +0300

    Добавлен проект библиотеки классов

--- LopushokDemo/LopushokDemo/ADO/ExtensionClasses/Product.cs
+++ LopushokDemo/LopushokDemo/ADO/ExtensionClasses/Product.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace LopushokDemo.ADO
 {
     public partial class Product
     {
+        public ImageSource ImageSource
+        {
+            get
+            {
+                var bitmap = new BitmapImage();
+                byte[] bytes;
+
+                if (Image == null || Image == "" || Image == "нет" || Image == "не указано")
+                {
+                    bytes = File.ReadAllBytes(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName +
+                                              $"\\Resources\\products\\picture.png");
+                }
+                else
+                {
+                    bytes = File.ReadAllBytes(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName +
+                                              $"\\Resources\\{Image}");
+                }
+
+                var ms = new MemoryStream(bytes);
+
+
+                bitmap.BeginInit();
+                bitmap.StreamSource = ms;
+                bitmap.EndInit();
+                return bitmap;
+            }
+        }
     }
 }
--- LopushokDemo/LopushokDemo/Pages/ProductsListPage.xaml.cs
+++ LopushokDemo/LopushokDemo/Pages/ProductsListPage.xaml.cs
@@ -22,11 +22,55 @@ namespace LopushokDemo.Pages
     public partial class ProductsListPage : Page
     {
         private List<Product> _products = new List<Product>();
+        private Func<Product, object> _sortingQuery = x => x.Name;
+        private Predicate<Product> _filterQuery = x => true;
+
         public ProductsListPage()
         {
             InitializeComponent();
             _products = App.Connection.Product.ToList();
             lvProducts.ItemsSource = _products;
+
+            var filters = new List<ProductType> { new ProductType { Name = "Все типы" } };
+            filters.AddRange(App.Connection.ProductType.ToList());
+            cbFilter.ItemsSource = filters;
+            cbFilter.SelectedIndex = 0;
+
+            var sorts = new List<string> {
+            "Без сортировки",
+             "Наименование", "Номер производственного цеха ",
+            "Минимальная стоимость для агента"};
+            cbSort.ItemsSource = sorts;
+            cbSort.SelectedIndex = 0;
+
+            cbSortOrders.ItemsSource = new List<string> { "По убыванию", "По возрастанию" };
+            cbSortOrders.SelectedIndex = 0;
+        }
+
+        private void ReloadData()
+        {
+            _products = App.Connection.Product
+            .ToList()
+            .Where(x => _filterQuery(x))
+                .OrderBy(x => _sortingQuery(x))
+                .ToList();
+            SearchData();
+        }
+
+        private void SearchData()
+        {
+            lvProducts.ItemsSource = _products
+                .Where(x => string.Join(" ", x.Name).ToLower().Contains(tbSearch.Text.ToLower()));
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SearchData();
+        }
+
+        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
