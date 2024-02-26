8299beba1bafd2269b03159a7afbbae472f47b9f
riazonbin <riazonbin@mail.ru>
2024-02-26 17:19:34 +0300

    Добавлены изображения, папка для страниц

--- LopushokDemo/LopushokDemo/MainWindow.xaml.cs
+++ LopushokDemo/LopushokDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LopushokDemo.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace LopushokDemo
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new ProductsListPage());
         }
     }
 }
